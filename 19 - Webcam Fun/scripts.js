24c87577f3421590b4378ea071d27d0769e329f0
anthonyg <agilbert@rr-solutions.us>
2018-12-10 19:44:09 -0600

    Completes Day 19: Webcam Fun

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,113 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch(err => {
+      console.error(`OH NO!!!`, err);
+    });
+}
+
+  function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    //now we will adjust to video to equal the paintToCanvas
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+      //now we take the image context and draw an images
+      ctx.drawImage(video, 0, 0, width, height);
+      //  take the pixels out of the canvas
+      let pixels = ctx.getImageData(0, 0, width, height);
+      // mess witht he pixels
+      // pixels = redEffect(pixels);
+      // pixels = rgbSplit(pixels);
+      pixels = greenScreen(pixels);
+      
+      //  put the pixels back
+      ctx.putImageData(pixels, 0, 0);
+    }, 16);
+  }
+
+
+
+function takePhoto() {
+  //  played the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  //take the data out of the canvas
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+// function redEffect(pixels) {
+//   for(let i = 0; i < pixels.data.length; i += 4) {
+//     pixels.data[1 + 0] = pixels.data[1 + 0]  + 100; //red
+//     pixels.data[i + 1] = pixels.data[i + 1] -50;    //green
+//     pixels.data[i + 2] = pixels.data[i + 2] * 0.5;  // blue
+//   }
+//   return pixels;
+// }
+
+
+function redEffect(pixels) {
+  for(let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[1 + 0] = pixels.data[1 + 0]  - 50; //red
+    pixels.data[i + 1] = pixels.data[i + 1] - 100 ;    //green
+    pixels.data[i + 2] = pixels.data[i + 2] + 200;  // blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for(let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; //red
+    pixels.data[i + 100] = pixels.data[i + 1]; //green
+    pixels.data[i - 150] = pixels.data[i + 2]; // blue
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  //first we create an empty 'levels' object that holds our minimum and maximum amount of green
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+        //take it out!
+        pixels.data[i + 3] = 0;
+      }
+  }
+  return pixels;
+}
+ 
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
